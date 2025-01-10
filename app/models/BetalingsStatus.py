384a917f4efe5ab73d3b653c9ef5191ac5cb83a0
6EmptyBucket9 <jarno.ewijk@gmail.com>
2025-01-10 18:52:52 +0100

    Working ORM Classes

--- app/models/BetalingsStatus.py
+++ app/models/BetalingsStatus.py
@@ -3,20 +3,6 @@ from app.models import db
 
 class BetalingsStatus(db.Model):
     __tablename__ = 'betalings_status'
+
     idBetalings_status = Column(Integer, primary_key=True)
     status = Column(String(45), nullable=False)
-
-    def get_idBetalings_status(self):
-        return self.idBetalings_status
-
-    def set_idBetalings_status(self, new_id):
-        self.idBetalings_status = new_id
-
-    def get_status(self):
-        return self.status
-
-    def set_status(self, new_status):
-        self.status = new_status
-
-    idBetalings_status = property(get_idBetalings_status, set_idBetalings_status)
-    status = property(get_status, set_status)
--- app/models/Medewerker.py
+++ app/models/Medewerker.py
@@ -1,10 +1,11 @@
 from sqlalchemy import Column, Integer, String, Date, ForeignKey
 from sqlalchemy.orm import relationship
 from app.models import db
-from app.models.Functies import Functie
+from app.models.Functies import Functies
 
 class Medewerker(db.Model):
     __tablename__ = 'medewerker'
+
     Medewerkernr = Column(Integer, primary_key=True)
     voornaam = Column(String(45), nullable=False)
     tussenvoegsel = Column(String(45), nullable=True)
@@ -13,26 +14,4 @@ class Medewerker(db.Model):
     functie = Column(Integer, ForeignKey('functies.ID'), nullable=False)
     in_dienst_sinds = Column(Date, nullable=False)
 
-    functie_rel = relationship('Functie', backref='medewerkers')
-
-    def get_medewerkernr(self):
-        return self.Medewerkernr
-
-    def set_medewerkernr(self, new_nr):
-        self.Medewerkernr = new_nr
-
-    def get_voornaam(self):
-        return self.voornaam
-
-    def set_voornaam(self, new_voornaam):
-        self.voornaam = new_voornaam
-
-    def get_achternaam(self):
-        return self.achternaam
-
-    def set_achternaam(self, new_achternaam):
-        self.achternaam = new_achternaam
-
-    medewerkernr = property(get_medewerkernr, set_medewerkernr)
-    voornaam = property(get_voornaam, set_voornaam)
-    achternaam = property(get_achternaam, set_achternaam)
+    functie_rel = db.relationship('Functies', backref='medewerkers', lazy='subquery')
